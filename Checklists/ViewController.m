b33d49b134a563cf3059967c8c5035a93546defc
forkbattery <forkbattery@gmail.com>
2014-12-26 20:09:01 -0500

    Setup initial table view controller

--- Checklists/ViewController.m
+++ Checklists/ViewController.m
@@ -24,4 +24,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 1;
+
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    return cell;
+    
+}
+
 @end
