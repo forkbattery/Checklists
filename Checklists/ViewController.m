d3c4286029cd2f941817889a86777b54a07d3796
forkbattery <forkbattery@gmail.com>
2014-12-27 21:46:04 -0500

    tap gesture recognized

--- Checklists/ViewController.m
+++ Checklists/ViewController.m
@@ -26,14 +26,53 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 1;
+    return 100;
 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    
+    UILabel * label = (UILabel *)[cell viewWithTag:1000];
+    
+    if (indexPath.row == 0) {
+        
+        label.text = @"Ice-cream";
+        
+    } else if (indexPath.row % 5 == 1) {
+        
+        label.text = @"Pizza";
+        
+    } else if (indexPath.row % 5 == 2) {
+        
+        label.text = @"Burger";
+        
+    } else if (indexPath.row % 5 == 3) {
+        
+        label.text = @"Tacos";
+        
+    } else if (indexPath.row % 5 == 4) {
+        
+        label.text = @"Noodles";
+        
+    }
+    
     return cell;
+    
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell * cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
 }
 
