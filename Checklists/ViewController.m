9f39ab883b95fc2a31aa0f69260c02da2208b36c
forkbattery <forkbattery@gmail.com>
2014-12-31 03:09:03 -0500

    Ability to delete item. Also + button added to add a fake or manual item. More advanced functionality to come

--- Checklists/ViewController.m
+++ Checklists/ViewController.m
@@ -20,6 +20,7 @@
 }
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     
     _items = [[NSMutableArray alloc] initWithCapacity:20];
@@ -66,6 +67,31 @@
 
 }
 
+- (void)configureCheckmarkForCell:(UITableViewCell *)cell withChecklistItem:(ChecklistItem *) item {
+    
+    //if the item is checked, display checkmark
+    if (item.checked) {
+        
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+    } else {
+        
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        
+    }
+    
+}
+
+
+- (void)configureTextForCell:(UITableViewCell *)cell withChecklistItem:(ChecklistItem *) item {
+    
+//  UILabel * label = (UILabel *)[cell viewWithTag:1000];
+//  label.text = item.text;
+    
+    cell.textLabel.text = item.text;
+    
+}
+
 
 //data source mathod no.2 to get the cell to display the row in the given index path
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -81,25 +107,6 @@
     
 }
 
-- (void)configureTextForCell:(UITableViewCell *)cell withChecklistItem:(ChecklistItem *) item {
-    
-    UILabel * label = (UILabel *)[cell viewWithTag:1000];
-    label.text = item.text;
-}
-
-- (void)configureCheckmarkForCell:(UITableViewCell *)cell withChecklistItem:(ChecklistItem *) item {
-    
-    if (item.checked) {
-        
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        
-    } else {
-        
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        
-    }
-}
-
 
 //delegate method to handle taps on rows
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -113,6 +120,33 @@
     [self configureCheckmarkForCell:cell withChecklistItem:item];
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+}
+
+- (IBAction)addItem {
+    
+    NSInteger newRowIndex = [_items count];
+    
+    ChecklistItem * item = [[ChecklistItem alloc]init];
+    item.text = @"I am a new row";
+    item.checked = YES;
+    [_items addObject:item];
+    
+    NSIndexPath * indexPath = [NSIndexPath indexPathForRow:newRowIndex inSection:0];
+    NSArray * indexPaths = @[indexPath];
+    
+    [self.tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+}
+
+//delete item by swiping the item to the left
+- (void) tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [_items removeObjectAtIndex:indexPath.row];
+    
+    NSArray * indexPaths = @[indexPath];
+    
+    [self.tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
     
 }
 
